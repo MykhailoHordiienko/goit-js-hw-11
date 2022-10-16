cc8a73624ad801e209d87a624bb4dbb37dc91401
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2022-10-16 15:56:29 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { fetchImages } from './js/fetch-images';
 import { renderGallery } from './js/render-gallery';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
-
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchForm = document.querySelector('#search-form');
